5d0a0657c120936fd98340581ba809dfbcf52048
Katrolli <ennio_cano@live.com>
2023-10-10 15:05:46 +0200

    worked on styling the bookComponent

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BookService } from './services/book.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css'],
 })
-export class BooksComponent implements OnInit {
+export class BooksComponent implements OnInit, OnDestroy {
   books: any = [];
   public CreateBookForm!: FormGroup;
   isCreateBookForm: boolean = false;
+  formError: string | null = null;
+  private destroy = new Subject<void>();
 
   constructor(private bookService: BookService) {}
 
@@ -34,6 +38,11 @@ export class BooksComponent implements OnInit {
         imageUrl: new FormControl('', Validators.required),
         category: new FormControl('', Validators.required),
       });
+      this.CreateBookForm.valueChanges
+        .pipe(takeUntil(this.destroy))
+        .subscribe(() => {
+          this.formError = null;
+        });
       this.isCreateBookForm = !this.isCreateBookForm;
     } else {
       this.isCreateBookForm = !this.isCreateBookForm;
@@ -41,18 +50,30 @@ export class BooksComponent implements OnInit {
   }
 
   submitForm() {
-    this.bookService
-      .createBook(
-        this.CreateBookForm.get('title')?.value,
-        this.CreateBookForm.get('description')?.value,
-        this.CreateBookForm.get('imageUrl')?.value,
-        this.CreateBookForm.get('category')?.value
-      )
-      .subscribe(
-        (data) => {
-          this.books.push(data);
-        },
-        (err) => {}
-      );
+    if (this.CreateBookForm.valid) {
+      this.bookService
+        .createBook(
+          this.CreateBookForm.get('title')?.value,
+          this.CreateBookForm.get('description')?.value,
+          this.CreateBookForm.get('imageUrl')?.value,
+          this.CreateBookForm.get('category')?.value
+        )
+        .subscribe(
+          (data) => {
+            this.books.push(data);
+          },
+          (err) => {
+            this.formError =
+              err.message || 'An error occurred while submitting the form.';
+            console.error(err);
+          }
+        );
+    } else {
+      this.formError = 'Please correct the form before submitting.';
+    }
+  }
+  ngOnDestroy() {
+    this.destroy.next();
+    this.destroy.complete();
   }
 }
--- src/app/helpers/token.interceptor.ts
+++ src/app/helpers/token.interceptor.ts
@@ -22,7 +22,6 @@ export class TokenInterceptor implements HttpInterceptor {
           Authorization: `Bearer ${this.authService.getToken()}`,
         },
       });
-      console.log(newRequest);
       return next.handle(newRequest);
     }
     return next.handle(req);
